659a798110ead25bc31a49f814224a42f01aa25a
Gustavo9915 <251703718@prepaenlinea-sep.edu.mx>
2026-05-02 11:35:41 -0600

    js agregado

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-alert("hola mundo");
+
+function playSonido(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    console.log(contador);
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio);
+    tecla.omclick = function (){
+        playSonido(idAudio);
+    };
+    tecla.onkeydown = function(evento){
+        if(evento.code === `Space` || evento.code ===  `enter`){
+            tecla.classList.add(`Activa`);
+        }
+        console.log(evento.code === `Space` || evento.cade === `enter`);
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.renove(`activa`);
+    }
+}
