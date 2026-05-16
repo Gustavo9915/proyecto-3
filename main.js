656903a3e3e7ffecbd2780924c309dea9757cc01
Gustavo9915 <251703718@prepaenlinea-sep.edu.mx>
2026-05-16 11:14:05 -0600

    sonido

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-
 function playSonido(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeTeclas = document.querySelectorAll('.tecla');
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     console.log(contador);
+
+
     const idAudio = `#sonido_${instrumento}`;
     console.log(idAudio);
-    tecla.omclick = function (){
+
+    tecla.onclick = function(){
         playSonido(idAudio);
     };
-    tecla.onkeydown = function(evento){
-        if(evento.code === `Space` || evento.code ===  `enter`){
-            tecla.classList.add(`Activa`);
-        }
-        console.log(evento.code === `Space` || evento.cade === `enter`);
-    }
 
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
+        }
+        console.log(evento.code === 'Space' ||  evento.code === 'Enter');
+
+
+    }
     tecla.onkeyup = function(){
-        tecla.classList.renove(`activa`);
+        tecla.classList.remove('activa')
     }
 }
